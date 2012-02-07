8e7ec38fdc22e89e2d764a8202d53b07f079aef3
Moacir <moa.moda@gmail.com>
2012-02-07 12:03:36 -0200

    adding instructions of configurations in oai-databases.php file

--- htdocs/isis-oai-provider/oai-databases-sample.php
+++ htdocs/isis-oai-provider/oai-databases-sample.php
@@ -1,19 +1,39 @@
 ;<?php /*
 
+; EXAMPLE:
+; This is a example of database configuration.
+
+; Name of database. (This needs to be a same name of database file, includes match cases, localized in DATABASE_PATH, setted in oai-config.php)
 ;[lilacs]
+
+; Name of set that isis-oai-provider will be showed in ListSets verb.
 ;name=LILACS
+
+; Description of set, that will be showed in ListSets verb.
 ;description="Latin American and Caribbean Health Sciences"
+
+; Path of directory that contains the database's files.
 ;path=/home/moa/project/bireme/vhost/isis-oai-provider/bases/isis-oai-provider
-;mapping=lilacs_dc.pft
+
+; Filename that contains the format that will be used to do the mapping of contents. (Located in wxis/pft/ directory)
+;mapping=lilacs.pft
+
+; Prefix insert in field date. (This need to be inserted in .fst file that will be used to make the invertion of database) 
 ;prefix=oai_date_
+
+; Key length of database invertion (LILACS is 16/60)
 ;isis_key_length=1660
+
+; Field in database that contains identifier information of register.
 ;identifier_field=2
 
+
+; Configuration of direve.(mst|xrf) database.
 [direve]
 name=direve
 description="Events directory"
 path=/home/projects/isis-oai-provider/bases/isis-oai-provider
-mapping=direve.i2x
+mapping=direve.ini
 prefix=oai_date_
 isis_key_length=1030
 identifier_field=10
